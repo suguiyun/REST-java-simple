cfde791f55264ccdd58978c749883a7196cad675
sugy <sugy@glodon.com>
2019-09-27 13:49:34 +0800

    v1.0分支

--- src/main/java/com/inklabsfoundation/utils/ByteUtil.java
+++ src/main/java/com/inklabsfoundation/utils/ByteUtil.java
@@ -21,4 +21,20 @@ public class ByteUtil {
 
         return sb.toString().trim();
     }
+
+    public static String toString(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length << 2);
+        byte[] var2 = b;
+        int var3 = b.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            byte x = var2[var4];
+            int hi = (x & 240) >> 4;
+            int lo = x & 15;
+            sb.append(HEX_CHARS[hi]);
+            sb.append(HEX_CHARS[lo]);
+        }
+
+        return sb.toString().trim();
+    }
 }
